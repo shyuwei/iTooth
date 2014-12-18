50a52a7beecfd182e0a89698e1e9a8b579068eeb
ShyuWei <shyuwei@gmail.com>
2014-12-18 22:16:25 +0800

    Initial Commit

---
+++ iTooth/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  iTooth
+//
+//  Created by ShyuWei on 2014/12/18.
+//  Copyright (c) 2014年 ShyuWei. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iTooth/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iTooth
+//
+//  Created by ShyuWei on 2014/12/18.
+//  Copyright (c) 2014年 ShyuWei. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
